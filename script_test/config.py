5e682fe3d9e5938276243c4280001760986f31c1
Suraj Sedai <surajsedai05@gmail.com>
2025-03-14 13:59:03 -0400

    hoping

--- script_test/config.py
+++ script_test/config.py
@@ -1,5 +1,5 @@
 # config.py
 
-ACR_HOST = "https://identify-us-west-2.acrcloud.com/v1/identify"  # Host URL
+ACR_HOST = "https://identify-us-west-2.acrcloud.com"  # Host URL
 API_KEY = "8d1c48689f6030b42cc828c5a2187995"  # Access Key
 API_SECRET = "RVn6mFgYU3HjjJ815YPuWIpwCuoYaqFBHZJ7devX"  # Secret Key
